51a2dc52fd1c534e1520dfb6a1d6be9b237fafad
David Jardin <d.jardin@djumla.de>
2026-01-17 10:35:21 +0100

    wip commit

---
+++ libraries/src/WebService/Resource/Attribute/Property/Description.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2ß26 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\WebService\Resource\Attribute\Property;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Description extends Attribute
+{
+    public function __construct(
+        string $description
+    ) {
+
+    }
+}
