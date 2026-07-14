c0ddeff7728868718851bc40b5c7c054e51b662d
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2026-07-14 18:06:01 +0200

    prepare content article dto

--- api/components/com_content/src/Resource/Enum/ArticleState.php
+++ api/components/com_content/src/Resource/Enum/ArticleState.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace Joomla\Component\Content\Api\Resource\Enum
+/**
+ * @package     Joomla.API
+ * @subpackage  com_content
+ *
+ * @copyright   (C) 2026 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Content\Api\Resource\Enum;
 
 enum ArticleState: int
 {
-    case Published = 1;
+    case Published   = 1;
     case Unpublished = 0;
-    case Trashed = -2;
-    case Archived = 2;
+    case Trashed     = -2;
+    case Archived    = 2;
 }
